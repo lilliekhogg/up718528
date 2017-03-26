2dfa3e725b73440f72414235beff1bae1cbe64ba
lilliekhogg <lilliekhogg@gmail.com>
2017-03-26 21:29:28 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -10,8 +10,16 @@ var sql = mysql.createConnection(config.mysql);
 
 var app = express();
 
+var http = require("http");
 
-app.listen(8080);
+http.createServer(function(request, response) {
+response.writeHead(200, {"Content-Type": "text/plain"});
+response.write("Hello World");
+response.end();
+})
+
+.listen(8080);
+
 
 //server functions
 

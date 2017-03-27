da753f93bb892f8f1cffba8f26164a63302f4270
lilliekhogg <lilliekhogg@gmail.com>
2017-03-27 20:41:42 +0100

    Add files via upload

--- src/qotd.js
+++ src/qotd.js
@@ -1,27 +1,38 @@
-function showQuote(){
+function displayQuote(){
 
-quotes = [];
-authors = [];
-quotes[0] = "I have a new philosophy. I'm only going to dread one day at a time.";
-authors[0] = "Charles Schulz";
-quotes[1] = "Reality is the leading cause of stress for those in touch with it.";
-authors[1] = "Jack Wagner";
-quotes[2] = "Few things are harder to put up with than the annoyance of a good example.";
-authors[2] = "Mark Twain";
-quotes[3] = "The pure and simple truth is rarely pure and never simple.";
-authors[3] = "Oscar Wilde";
-quotes[4] = "There's no business like show business, but there are several businesses like accounting.";
-authors[4] = "David Letterman";
-quotes[5] = "Man invented language to satisfy his deep need to complain.";
-authors[5] = "Lily Tomlin";
+quote = [];
+quoter = [];
+quote[0] = "Believe you can and you're halfway there";
+quoter[0] = "Theodore Roosevelt";
+quote[1] = "If oppertunity doesnt knock, build a door";
+quoter[1] = "Milton Berle";
+quote[2] = "The pure and simple truth is rarely pure and never simple.";
+quoter[2] = "Oscar Wilde";
+quote[3] = "The secret of getting ahead is getting started";
+quoter[3] = "Mark Twain";
+quote[4] = "If you dream it, you can do it.";
+quoter[4] = "Walt Disney";
+quote[5] = "You must be the change you wish to see in the world.";
+quoter[5] = "Mahatma Gandhi";
+quote[6] = "If you judge people, you have no time to love them.";
+quoter[6] = "Mother Teresa";
+quote[7] = "It is our choices that show what we truely are, far more than our abilities.";
+quoter[7] = "JK Rowling";
+quote[8] = "If you can't make it good, at least make it look good.";
+quoter[8] = "Bill Gates";
+quote[9] = "It always seems impossible until its done";
+quoter[9] = "Nelson Mandela";
+quote[10] = "Happiness can exist only in acceptance.";
+quoter[10] = "George Orwell";
 
-//calculate a random index number
-index = Math.floor(Math.random() * quotes.length);
 
-//display the quotation
+//calculate a random quote to display on dashboard
+randomQOTD = Math.floor(Math.random() * quote.length);
+
+//display the quotation and the quote author
 
 
-document.getElementById("MyQuotes").textContent = "Quote of the Day: " + quotes[index] + "-" + authors[index];
+document.getElementById("MyQuotes").textContent = "Quote of the Day: " + quote[randomQOTD] + "-" + quoter[randomQOTD];
 }
 
-showQuote();
+displayQuote();
